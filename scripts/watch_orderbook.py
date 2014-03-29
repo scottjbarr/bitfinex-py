44f9deb3ffbf837f57703c77ccbadf5fe5217ae6
Scott Barr <scott@signalvsnoise.com>
2014-03-29 15:10:20 +1030

    Added scripts/watch_orderbook.py

---
+++ scripts/watch_orderbook.py
@@ -0,0 +1,35 @@
+import os, sys
+from datetime import datetime
+
+import bitfinex
+
+# symbol to query the order book
+symbol = 'btcusd'
+
+# set the parameters to limit the number of bids or asks
+parameters = {'limit_asks': 5, 'limit_bids': 5}
+
+# create the client
+client = bitfinex.Client()
+
+while True:
+
+    # get latest ticker
+    ticker = client.ticker(symbol)
+
+    # get the orderbook
+    orders = client.orderbook(symbol, parameters)
+
+    # clear the display, and update values
+    os.system('clear')
+
+    print("# Bitfinex (Last Update : %s)" % (datetime.now()))
+    print("## Last Ticker")
+    print(ticker)
+
+    for order_type in orders:
+        print("")
+        print("%s %s" % ("## ", order_type))
+        for order in orders[order_type]:
+            print order
+
