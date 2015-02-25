bbaf81e891c83df4171343d6e0df1f0425c88269
Scott Barr <scottjbarr@gmail.com>
2015-02-26 01:23:00 +1000

    Bump to version 0.2.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ dependencies = [
 ]
 
 setup(name='bitfinex',
-    version='0.2.3',
+    version='0.2.4',
     description = 'Python client for the Bitfinex API',
     author = 'Scott Barr',
     author_email = 'scottjbarr@gmail.com',
@@ -16,7 +16,7 @@ setup(name='bitfinex',
     packages=['bitfinex'],
     scripts = ['scripts/watch_orderbook'],
     install_requires = dependencies,
-    download_url = 'https://github.com/scottjbarr/bitfinex/tarball/0.2.3',
+    download_url = 'https://github.com/scottjbarr/bitfinex/tarball/0.2.4',
     keywords = ['bitcoin', 'btc'],
     classifiers = [],
     zip_safe=True)
