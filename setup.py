f7090ead3a82aacde23464617f98a3ad9f488a59
Scott Barr <scottjbarr@gmail.com>
2015-02-26 00:17:26 +1000

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bitfinex',
-    version='0.1.0',
+    version='0.2.0',
     description = 'Python client for the Bitfinex API',
     author = 'Scott Barr',
     author_email = 'scottjbarr@gmail.com',
